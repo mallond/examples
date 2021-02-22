9a8010146b3ea55437895051f8976780a95eff15
David Mallon ⚙ <mallond@gmail.com>
2021-02-22 09:05:41 -0800

    Update jtest.js

--- jtest.js
+++ jtest.js
@@ -7,7 +7,7 @@ var capabilities = Capabilities.firefox();
         .build();
     try {
       await driver.get('http://www.google.com/ncr');
-      await driver.findElement(By.name('qxxx')).sendKeys('webdriver', Key.RETURN);
+      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
       await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
       console.log('done');
     } finally {
